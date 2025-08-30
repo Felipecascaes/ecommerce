79e5575a2a1027a4d2d4475d7b5b4af415e1061e
Felipecascaes <fcascaesfernandes@gmail.com>
2025-08-29 22:01:17 -0300

    primeiro commit

--- poo/src/main/java/com/superior/poo/PooApplication.java
+++ poo/src/main/java/com/superior/poo/PooApplication.java
@@ -2,12 +2,29 @@ package com.superior.poo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+
 public class PooApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PooApplication.class, args);
 	}
 
+    /*
+    HTTP <> != HTTPS  -  Se um site não tiver HTTPS://  -  Desconfie
+
+    Get Buscar as informações ou Pegar
+    Post Enviar as informações ou Empurrar
+    Put Alterar as informações ou Mudar
+    Patch Alterar as informações ou Mudar
+    Delete Deletar as informações
+     */
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
 }
